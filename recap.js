92b56daff185f3429bd52083e54501a7cfba71b6
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-01-31 18:06:56 +0600

    first commit

---
+++ recap.js
@@ -0,0 +1,76 @@
+//1. variable
+
+
+var myName = ('saju')
+
+
+//2. array 
+
+
+var friend = ['abul', 'babul','dabul','ebul','golgol']
+
+
+
+// < ,> ,<=, >=, !=, =, ==, !==, ETC 
+
+
+// 3. Condition 
+
+if(friend.length <2){
+        console.log ("fokir tor kunu friend nai")
+}
+else{
+
+}
+
+
+// 4. loop 
+
+var number = 0;
+while(number <=6){
+    console.log(number);
+    number++;
+}
+
+
+for(var i = 0; i <= 10; i++){
+
+}
+
+
+//5. function 
+
+
+
+function isMoonUp(time){
+        if(time <7){
+            return true;
+        }
+        
+}
+
+
+//6.  object 
+
+
+var janTus = {
+        higth:60,
+        width:50,
+        baperTaka:'borolox',
+        bariGor:'Onek ase'
+
+}
+
+// 7. constant 
+
+
+
+let age = 17;
+age = 17;
+
+const countryName = 'bangladesh'
+
+
+console.log(age , countryName);
+
+
